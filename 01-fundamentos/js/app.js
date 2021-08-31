e9c39c61c66d62263ba38eaa591e039215d6888a
Jhon-Contreras <jhon.contreras13@gmail.com>
2021-08-31 15:17:22 -0400

    alert, prompt, confirm

---
+++ 01-fundamentos/js/app.js
@@ -0,0 +1,24 @@
+// alert('Hola desde app.js')
+console.log('Hola Mundo desde consola');
+
+
+//IMPORTANTE: Los valores de una constantes no pueden re asignarse 
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+// console.log('%c Mis variables', 'color: blue; font-weight: bold;');
+// console.log({a});
+// console.log({b});
+// console.log({c});
+
+// console.table({a,b,c,d,x})
+
+
+let outerWidth = 1000000;
+let outerHeight = 600;
+
