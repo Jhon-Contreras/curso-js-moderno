155a1de823d90002f3174d24205bc4fbc39b3fac
Jhon-Contreras <jhon.contreras13@gmail.com>
2021-08-31 21:05:11 -0400

    Return en funciones y funciones de flecha

--- 01-fundamentos/js/funciones.js
+++ 01-fundamentos/js/funciones.js
@@ -1,9 +1,9 @@
 // funciones 
 
 // Basicas 
-// function saludar(){
-//     console.log('Hola mundo');
-// }
+function saludar(){
+    console.log('Hola mundo');
+}
 
 // Anonima 
 const saludar2 = function (nombre){
@@ -15,6 +15,10 @@ function saludar(nombre){
     // objeto implicito arguments: Hace referencia a todos los elementos enviados a la funcion
     console.log(arguments);
     console.log('Hola ' + nombre);
+    return 1;
+
+    // esto no se ejecutará
+    console.log('Soy un codigo despues del return');
 }
 
 // funciones de flecha o lambda functions 
@@ -25,6 +29,40 @@ const saludarFlecha = ( nombre )=>{
 
 
 
-saludar('Jhon', 49, true, 'Chile');
-saludar2('Jhon');
-saludarFlecha('Jhon');
+const retornoDeSaludar = saludar('Jhon', 49, true, 'Chile');
+console.log(retornoDeSaludar);
+// saludar2('Jhon');
+// saludarFlecha('Jhon');
+
+
+// return 
+
+function sumar(a, b){
+    return a + b;
+
+}
+// transformar a function de flecha 
+
+// const sumar2 = (a,b)=>{
+//     return a + b;
+// }
+
+// resumir funcion de flecha cuando solo hay 1 return
+const sumar2 = (a,b) => a + b;
+
+
+console.log(sumar(5,2));
+console.log(sumar2(10,20));
+
+
+function getAleatorio(){
+    return Math.random();
+}
+
+console.log(getAleatorio());
+
+// transformar funcion getAleatorio en funcion de flecha sin llaves 
+
+const getAleatorio2 = () => Math.random();
+
+console.log(getAleatorio2());
