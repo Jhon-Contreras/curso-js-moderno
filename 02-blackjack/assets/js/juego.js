d2bf773b235c2fc542052e48552a508b78799e8e
Jhon-Contreras <jhon.contreras13@gmail.com>
2021-09-10 20:22:35 -0300

    Funcion para pedir una carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -13,22 +13,23 @@ const tiposCartas = ['C', 'D', 'H', 'S'];
 const especiales  = ['A', 'J', 'Q', 'K'];
 // funciona para crear deck de cartas 
 const crearDeck = () =>{
-    for( let i = 2; i <=10; i ++ ){ //el ciclo for inicializa en 2 (las cartas parten del 2), debe ser menor o igual a 10 (las cartas llegan hasta el numero 10)
+    for( let i = 2; i <=10; i++ ){ //el ciclo for inicializa en 2 (las cartas parten del 2), debe ser menor o igual a 10 (las cartas llegan hasta el numero 10)
         
         // Creamos un for of para asignar los numeros a cada tipo de carta 
         for (let tipo of tiposCartas) {
            deck.push(i + tipo) //agregamos las cartas al array del deck
         }
-        // creamos un for para las cartas especiales y asignarlas a cada tipo de Carta
-        for( let tipo of tiposCartas ){
-            for( let especial of especiales ){
-                deck.push(especial + tipo);
-            }
-        }
-        
     }
+    // creamos un for para las cartas especiales y asignarlas a cada tipo de Carta
+    for( let tipo of tiposCartas ){
+        for( let especial of especiales ){
+            deck.push(especial + tipo);
+        }
+    }
+        
+   
 
-    console.log(deck);
+    // console.log(deck); // deck ordenado
     deck = _.shuffle( deck ); //creamos una baraja aleatoria con la libreria underscore.js
     console.log(deck); //imprimimos nuevamente el deck barajeado
     return deck;
@@ -38,10 +39,22 @@ const crearDeck = () =>{
 
 crearDeck();
 
+// Esta funcion permite tomar una carta
+
+const pedirCarta = () =>{
+
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck' //error en consola
+    }
+    const carta = deck.pop(); //remueve el ultimo elemento del arreglo
+
+    console.log(deck);
+    console.log(carta); // carta extraida del deck con .pop()
+    
+}
 
 
-
-
+pedirCarta();
 
 
 
