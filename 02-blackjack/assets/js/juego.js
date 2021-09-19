59897f66e6d783191b5b797a159a973911699cef
Jhon-Contreras <jhon.contreras13@gmail.com>
2021-09-18 21:16:08 -0300

    Optimizaciones de codigo

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -1,6 +1,6 @@
 // sintaxis patron modulo 
 // funcion anonima auto invocada
-(()=>{
+const miModulo = (()=>{
 
     'use strict' // modo estricto
     
@@ -15,29 +15,48 @@
 
     // inicializar el deck de cartas 
     let deck          = [];
-    const tiposCartas = ['C', 'D', 'H', 'S'];
-    const especiales  = ['A', 'J', 'Q', 'K'];
+    const tiposCartas = ['C', 'D', 'H', 'S'],
+          especiales  = ['A', 'J', 'Q', 'K'];
 
     // puntos del jugador 
-    let puntosJugador = 0;
+    // let puntosJugador = 0,
     // puntos del com 
-    let puntosComputadora = 0;
+        // puntosComputadora = 0;
+    // (Optimizacion) puntos jugadores 
+    let puntosJugadores = [];
 
     // botones acciones 
-    const btnPedir      = document.querySelector('#btnPedirCarta');
-    const btnNuevoJuego = document.querySelector('#btnNuevoJuego');
-    const btnDetener    = document.querySelector('#btnDetener');
+    const btnPedir      = document.querySelector('#btnPedirCarta'),
+          btnNuevoJuego = document.querySelector('#btnNuevoJuego'),
+          btnDetener    = document.querySelector('#btnDetener');
 
     // etiquetas small con puntos 
-    const puntosHtml = document.getElementsByTagName('small');
+    const puntosHtml = document.querySelectorAll('small');
 
-    // referencia a divs cartas 
-    const divCartasJugador     = document.querySelector('#jugador-cartas');
-    const divCartasComputadora = document.querySelector('#computadora-cartas');
+    // referencia a divs cartas jugadores
+    const divCartasJugadores = document.querySelectorAll('.divCartas');
+         
 
+    
+    // (Optimizacion) esta funcion es para inicializar el juego 
+    const inicializarJuego = ( numJugadores = 2  ) =>{
+        deck = crearDeck();
+        puntosJugadores = [];
+        for(let i = 0; i< numJugadores; i++){
+            puntosJugadores.push(0);
+        }
+       
+        puntosHtml.forEach( elem => elem.innerText = 0 ); // borramos los puntajes en el html 
+        divCartasJugadores.forEach ( elem => elem.innerHTML = ' '); // reinicializamos las cartas html
 
-    // funciona para crear deck de cartas 
+        btnPedir.disabled   = false; //activamos boton pedirCarta
+        btnDetener.disabled = false; //activamos boton detener
+    }
+
+    // funcion para crear deck de cartas 
     const crearDeck = () =>{
+
+        deck = []; //(Optimizacion) reiniciamos el deck
         for( let i = 2; i <=10; i++ ){ //el ciclo for inicializa en 2 (las cartas parten del 2), debe ser menor o igual a 10 (las cartas llegan hasta el numero 10)
             
             // Creamos un for of para asignar los numeros a cada tipo de carta 
@@ -51,18 +70,12 @@
                 deck.push(especial + tipo);
             }
         }
-            
-    
 
-        // console.log(deck); // deck ordenado
-        deck = _.shuffle( deck ); //creamos una baraja aleatoria con la libreria underscore.js
-
-        return deck;
+        return _.shuffle( deck ); //(Optimizacion) creamos y retornamos una baraja aleatoria con la libreria underscore.js
 
     }
 
 
-    crearDeck();
 
     // Esta funcion permite tomar una carta
 
@@ -71,16 +84,14 @@
         if (deck.length === 0) {
             throw 'No hay cartas en el deck' //error en consola
         }
-        const carta = deck.pop(); //remueve el ultimo elemento del arreglo
+        return deck.pop(); //(Optimizacion) retornamos el ultimo elemento del arreglo
 
     
-        return carta;
     }
 
 
-    pedirCarta();
 
-    // cual es el valor de la carta 
+    // esta funcion sirve para obtener cual es el valor de la carta 
     const valorCarta = ( carta ) => {
         
         // extraer el primer valor del string carta
@@ -92,36 +103,30 @@
     
     }
 
-    //Turno de la computadora
 
-    const turnoComputadora = ( puntosMinimos )=>{
-        // Ejecutamos el ciclo una unica vez 
-        do {
+    // funcion para acumular puntos
+    // Turno: 0 = primer jugador y el ultimo será la computadora  
+    const acumularPuntos = ( carta, turno) =>{
+ 
+        puntosJugadores[turno] = puntosJugadores[turno] + valorCarta( carta );
+        puntosHtml[turno].innerText = puntosJugadores[turno];
+        return puntosJugadores[turno];
+    }
 
-            // tomar una carta al hacer click
-            const carta = pedirCarta();
+    // funcion para crear carta en html 
+    const crearCarta = ( carta, turno ) =>{
+        const imgCarta     = document.createElement('img');
+        imgCarta.src       = `assets/img/cartas/${carta}.png`; //carta dinamica
+        imgCarta.className = 'cartaImg'; // clase de las etiquetas img
+        // imgCarta.classList.add('cartaImg'); // otra alternativa para agregar clase
+        divCartasJugadores[turno].append( imgCarta ); // agrega la carta al html segun el turno
+    }
 
-            // puntos de la computadora
-            puntosComputadora = puntosComputadora + valorCarta( carta );
-            // console.log(puntosComputadora);
-            
-            puntosHtml[1].innerText = puntosComputadora;
-            
-            // se crea la etiqueta img 
-            const imgCarta     = document.createElement('img');
-            imgCarta.src       = `assets/img/cartas/${carta}.png`; //carta dinamica
-            imgCarta.className = 'cartaImg'; // clase de las etiquetas img
-            // imgCarta.classList.add('cartaImg'); // otra alternativa para agregar clase
-            divCartasComputadora.append( imgCarta );
-        
-            if (puntosMinimos > 21) {
-            
-                break;
-            }
-        } while ( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21)); // el ciclo se hará siempre que puntosComputadora sea menores a puntosMinimos y puntos Minimos sea menores o igual a 21
-        
-        // mensajes victoria, derrota y empate
-        setTimeout(()=>{
+    // funcion para determinar ganador 
+    const determinarGanador = () =>{
+        const [ puntosMinimos, puntosComputadora] = puntosJugadores;
+         // mensajes victoria, derrota y empate
+         setTimeout(()=>{
             if ( puntosComputadora === puntosMinimos ) { // si los puntos son iguales, empate
                 swal(":(", "Nadie gana", "error");
             } else if( puntosMinimos > 21){ // si los puntos minimos del jugador son mayores a 21, computadora gana
@@ -132,12 +137,24 @@
                 swal("Computadora gana", "Manco!!!!", "error"); //cualquier otra condición, es victoria de la computadora
             }
         }, 10);
+    }
+    //Turno de la computadora
+    const turnoComputadora = ( puntosMinimos )=>{
+        // Ejecutamos el ciclo una unica vez 
+        let puntosComputadora = 0;
+        do {
+
+            // tomar una carta al hacer click
+            const carta = pedirCarta();
+            puntosComputadora = acumularPuntos(carta, puntosJugadores.length - 1); // acumulamos los puntos de la computadora
+            crearCarta( carta, puntosJugadores.length - 1);
+        
+        } while ( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21)); // el ciclo se hará siempre que puntosComputadora sea menores a puntosMinimos y puntos Minimos sea menores o igual a 21
+        
+        determinarGanador();
     
         
-        
     }
-
-
 
 
     // EVENTOS mediante addEventListener 
@@ -145,20 +162,10 @@
     btnPedir.addEventListener('click', () => { //un callback, es una funcion ()=> que se envia como argumento
         // tomar una carta al hacer click
         const carta = pedirCarta();
-
-        // incrementar los puntos al hacer click
-        puntosJugador = puntosJugador + valorCarta( carta );
-        // console.log(puntosJugador);
+        const puntosJugador = acumularPuntos(carta, 0); // acumulamos los puntos del primer jugador
         
-        puntosHtml[0].innerText = puntosJugador;
+        crearCarta( carta, 0 );
         
-        // creamos la etiqueta img 
-        const imgCarta     = document.createElement('img');
-        imgCarta.src       = `assets/img/cartas/${carta}.png`; //carta dinamica
-        imgCarta.className = 'cartaImg'; // clase de las etiquetas img
-        // imgCarta.classList.add('cartaImg'); // otra alternativa para agregar clase
-        divCartasJugador.append( imgCarta );
-
         // condiciones 
         // evaluamos si el jugador tiene mas de 21 puntos 
         if (puntosJugador > 21) {
@@ -181,25 +188,21 @@
     btnDetener.addEventListener('click', ()=>{
         btnPedir.disabled   = true; //bloqueamos boton pedirCarta
         btnDetener.disabled = true; //bloqueamos boton detener
-        turnoComputadora( puntosJugador );
+        turnoComputadora( puntosJugadores[0] ); //asigamos los puntos a la computadora
     });
 
 
     // Evento nuevo juego
     btnNuevoJuego.addEventListener('click', ()=>{
-
-        deck = []; //vaciamos el deck
-        deck = crearDeck(); // creamos un nuevo deck
-        puntosJugador = 0; // vaciamos los puntos
-        puntosComputadora = 0; // vaciamos los puntos
-        puntosHtml[0].innerText = 0; // borramos el puntaje en el html jugador
-        puntosHtml[1].innerText = 0; // borramos el puntaje en el html computadora
-        divCartasComputadora.innerHTML = ' '; // quitamos las imagenes de las cartas
-        divCartasJugador.innerHTML = ' '; // quitamos las imagenes de las cartas
-        btnPedir.disabled   = false; //activamos boton pedirCarta
-        btnDetener.disabled = false; //activamos boton detener
+       
+        inicializarJuego();
+   
     })
 
+    // en el patron moludo, lo que sea que se agregue en el return será publico y podrá usarse externamente mediante el modulo
+    return {
+        nuevoJuego: inicializarJuego
+    };
 
 })();
 
