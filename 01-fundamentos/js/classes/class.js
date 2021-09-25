76c634576c45ee36a90fdae69695106cdc13c4bc
Jhon-Contreras <jhon.contreras13@gmail.com>
2021-09-25 14:37:34 -0300

    sets y gets

--- 01-fundamentos/js/classes/class.js
+++ 01-fundamentos/js/classes/class.js
@@ -1,10 +1,20 @@
 
 // palabra reservada class, buena practica con upper camelcase 
 class Persona {
+
+    /*
+    orden en una clase:
+    1° Propiedades y metodos estaticos
+    2° constructor
+    3° sets y gets
+    4° metodos
+    */ 
+
     // propiedades de clase 
     nombre = ' ';
     codigo = ' ';
-    frase = ' ';
+    frase  = ' ';
+    comida = ' ';
 
 
     //constructor (es un metodo que se ejecutará en el momento que se crea una nueva instancia de persona)
@@ -16,6 +26,17 @@ class Persona {
         this.frase  = frase;    
     }
 
+    // set y get 
+    //usualmente el set solo recibe un argumento, 
+    // set: Es para establecer un valor 
+    set setComidaFavorita( comida ){
+        this.comida = comida.toUpperCase();
+    }
+
+    // get: Es para recuperar un valor, idealmente el get debe retornar algo
+    get getComidaFavorita( ){
+        return `La comida favorita de ${ this.nombre } es: ${ this.comida }`;
+    }
     // metodos 
     quienSoy(){
         console.log(`Soy ${ this.nombre } y mi identidad es ${ this.codigo }`);
@@ -30,8 +51,14 @@ class Persona {
 
 const spiderMan = new Persona('Peter Parker', 'Spider Man', 'Soy tu amigable vecino spidy');
 const ironMan = new Persona('Tony Stark', 'Iron Man', 'Soy Iron Man');
-console.log(spiderMan);
-console.log(ironMan);
+
 
 spiderMan.miFrase();
-ironMan.miFrase();
+// ironMan.miFrase();
+
+// si es un set, no se llama con parentesis 
+spiderMan.setComidaFavorita = 'El pie de cereza de la tía May';
+
+
+console.log(spiderMan);
+console.log(spiderMan.getComidaFavorita);
