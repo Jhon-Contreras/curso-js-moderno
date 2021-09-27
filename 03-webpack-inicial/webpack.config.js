dba956c4a9694b3bd2987bbc1a20b749abdc1943
Jhon-Contreras <jhon.contreras13@gmail.com>
2021-09-27 20:21:37 -0300

    primeras configuraciones webpack

---
+++ 03-webpack-inicial/webpack.config.js
@@ -0,0 +1,25 @@
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+// archivo de configuracion webpack 
+module.exports = {
+    mode : 'development',
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options:{
+                    sources: false,
+                    minimize: false // html minimizado= true
+                }
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            inject: 'body'
+        }),
+    ]
+}
