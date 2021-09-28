f11cdc8fbdf13ad89393e850ccb5c77574c47275
Jhon-Contreras <jhon.contreras13@gmail.com>
2021-09-27 21:20:54 -0300

    resubiendo modulos 1 y 2 del curso

---
+++ 01-fundamentos/js/bases/arreglos.js
@@ -0,0 +1,17 @@
+// arreglos 
+
+let videoJuegos = ['GTA', 'NFS', 'COD'];
+console.log({videoJuegos});
+console.log(videoJuegos[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    'Texto',
+    1+2,
+    function(){},
+    ()=>{},
+    {a: 1},
+    ['CJ', 'Tommy', 'Calavera']
+];
+console.log(arregloCosas[7][2]);
---
+++ README.md
@@ -0,0 +1,2 @@
+# curso-js-moderno
+Curso de Javascript Moderno impartido en Udemy por el profesor Fernando Herrera
